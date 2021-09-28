04a6cded92ca828b9194277bd1e701592afabe19
sunil-edu <sunil@myeduate.com>
2021-09-28 10:09:26 +0530

    resolved fix

--- resolvers/resolver.go
+++ resolvers/resolver.go
@@ -1,7 +1,21 @@
 package resolvers
 
+import (
+	"myeduate"
+	"myeduate/ent"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{ client *ent.Client }
+
+// NewSchema creates a graphql executable schema.
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return myeduate.NewExecutableSchema(myeduate.Config{
+		Resolvers: &Resolver{client},
+	})
+}
